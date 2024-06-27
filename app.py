30f355b51bb0eb1ded418ee268f637bb04bfa678
SadineniAbhi <abhisadineni@gmail.com>
2024-06-27 20:51:59 +0530

    added dependencies

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, jsonify, request
 from rag.chatbot import get_response
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route('/response', methods=['GET'])
 def response():
     data = request.get_json()
